73d544982224a862be1f9b2756d9cd9bbb3064a6
Jake Bladt <jake@jakebladt.com>
2019-10-14 21:36:26 -0400

    add components

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ namespace runoncentos
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseWebRoot("wwwroot")
                 .UseStartup<Startup>();
     }
 }
